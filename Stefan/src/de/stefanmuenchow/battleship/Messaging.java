2dce64337a646dd9fd89d321d05df78473a5c881
Stefan Münchow <stefan.muenchow@haw-hamburg.de>
2013-04-22 21:28:14 +0200

    Stefan: Prepared eclipse project. Prepared some base classes.

---
+++ Stefan/src/de/stefanmuenchow/battleship/Messaging.java
@@ -0,0 +1,12 @@
+package de.stefanmuenchow.battleship;
+
+public class Messaging {
+	
+	public Messaging() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean join(String roomName, String playerName) {
+		return false;
+	}
+}
---
+++ Stefan/src/de/stefanmuenchow/battleship/SquareType.java
@@ -0,0 +1,5 @@
+package de.stefanmuenchow.battleship;
+
+public enum SquareType {
+	Water, Ship, Unknown
+}
