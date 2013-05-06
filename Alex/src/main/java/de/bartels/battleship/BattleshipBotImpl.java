db0ea34bc1507102522d645f9eecf4a80d4ef86b
alexanderbartels <bartels.xander@gmail.com>
2013-05-06 23:20:12 +0200

    foobar

---
+++ Alex/src/main/java/de/bartels/battleship/BattleshipBotImpl.java
@@ -0,0 +1,24 @@
+package de.bartels.battleship;
+
+class BattleshipBotImpl implements BattleshipBot {
+
+	
+	
+	public BattleshipBotImpl(final BattleshipBot.Builder builder) {
+		
+		// TODO
+		
+	}
+	
+	@Override
+	public void startup() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void shutdown() {
+		// TODO Auto-generated method stub
+		
+	}
+}
